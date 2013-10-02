db224d37b1e4e60c2a94c8d9f213bbbc7ce4f3d7
David Noel <dave.noel@gmail.com>
2013-10-01 21:44:31 -0500

    fuck

--- spatula.js
+++ spatula.js
@@ -1,7 +1,11 @@
-Spatula = {
-  'menu': function() {
+var $ = require('cheerio');
+
+var Spatula = {
+  'menu': function(url) {
+    var paths = Array.prototype.slice.call(arguments,1);
   },
   'scrape': function(save, menu, template, parser) {
+    parser = parser || Spatula.markdown;
   },
   'markdown': function(html) {
   }
